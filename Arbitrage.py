4f12b3baf0895cd6c3149e9a270f1df2579bc023
Steve Kipp <sakqm2@mst.edu>
2015-12-07 22:22:01 -0600

    added bid and ask prices, and trade account value on each exchange

--- Arbitrage.py
+++ Arbitrage.py
@@ -22,13 +22,15 @@ bitstamp = 'https://www.bitstamp.net/api/ticker/'
 class Exchange:
     'a common class for bitcoin exchanges'
 
-    def __init__(self, name, dataURL, walletURL, apiKey, fee):
+    def __init__(self, name, dataURL, walletURL, apiKey, fee, tradeAccountBalance):
         self.name = name
         self.dataURL = dataURL
         self.walletURL = walletURL
         self.apiKey = apiKey
         self.fee = fee
-        self.price = 0
+        self.tradeAccountBalance = tradeAccountBalance
+        self.bid = 0
+        self.ask = 0
 
 
 
@@ -36,23 +38,23 @@ class Exchange:
 coinbase = Exchange('Coinbase',
                     'https://api.exchange.coinbase.com/products/BTC-USD/book',
                     0,
-                    0, .0025) #account created
+                    0, .0025, 100) #account created
 bitfinex = Exchange('Bitfinex',
                     'https://api.bitfinex.com/v1/pubticker/btcusd',
                     0,
-                    0, .0020) #account created
+                    0, .0020, 100) #account created
 btc_e = Exchange('BTC_E',
                  'https://btc-e.com/api/3/ticker/btc_usd',
                  0,
-                 0, .0020) #missing account
+                 0, .0020, 100) #missing account
 OKCoin = Exchange('OKCoin',
                   'https://www.okcoin.com/api/ticker.do?symbol=btc_usd&ok=1',
                   0,
-                  0, .0020) #missing account
+                  0, .0020, 100) #missing account
 bitstamp = Exchange('Bitstamp',
                     'https://www.bitstamp.net/api/ticker/',
                     0,
-                    0, .0025) #missing account
+                    0, .0025, 100) #missing account
 
 
 def getCurrentData():
@@ -64,20 +66,30 @@ def getCurrentData():
     #global OKCoin
     #global bitstamp
 
-    coinbase.price = requests.get(coinbase.dataURL).json()['bids'][0][0]
-    bitfinex.price = requests.get(bitfinex.dataURL).json()['bid']
-    btc_e.price = requests.get(btc_e.dataURL).json()['btc_usd']['buy']
-    OKCoin.price = requests.get(OKCoin.dataURL).json()['ticker']['buy']
+    coinbase.bid = requests.get(coinbase.dataURL).json()['bids'][0][0]
+    bitfinex.bid = requests.get(bitfinex.dataURL).json()['bid']
+    btc_e.bid = requests.get(btc_e.dataURL).json()['btc_usd']['buy']
+    OKCoin.bid = requests.get(OKCoin.dataURL).json()['ticker']['buy']
     #cs = requests.get(coinsetter)
-    bitstamp.price = requests.get(bitstamp.dataURL).json()['bid']
+    bitstamp.bid = requests.get(bitstamp.dataURL).json()['bid']
 
-    print "Coinbase: "+coinbase.price
-    print "Bitfinex: "+bitfinex.price
-    print "BTC_E: "+str(btc_e.price)
-    print "OKCoin: "+OKCoin.price
+    #this will be really shitty latency but I'm doing it for test purposes
+    coinbase.ask = requests.get(coinbase.dataURL).json()['asks'][0][0]
+    bitfinex.ask = requests.get(bitfinex.dataURL).json()['ask']
+    btc_e.ask = requests.get(btc_e.dataURL).json()['btc_usd']['sell']
+    OKCoin.ask = requests.get(OKCoin.dataURL).json()['ticker']['sell']
+    #cs = requests.get(coinsetter)
+    bitstamp.ask = requests.get(bitstamp.dataURL).json()['ask']
+
+
+
+    print "Coinbase -> bid: "+coinbase.bid+" ask: "+coinbase.ask
+    print "Bitfinex -> bid: "+bitfinex.bid+" ask: "+bitfinex.ask
+    print "BTC_E -> bid: "+str(btc_e.bid)+" ask: "+str(btc_e.ask)
+    print "OKCoin -> bid: "+OKCoin.bid+" ask: "+OKCoin.ask
     #Coinsetter was getting strangely precise returns like 365.0 367.0 etc...
     #print "Coinsetter: "+str(cs.json()['bid']['price'])
-    print "Bitstamp: "+bitstamp.price
+    print "Bitstamp -> bid: "+bitstamp.bid+" ask: "+bitstamp.ask
     print '\n'
 
     exchanges = [coinbase, bitfinex, btc_e, OKCoin, bitstamp]
@@ -123,25 +135,29 @@ def tick(sc):
 
 
 def calculateBuyDifference(exchangeData):
-    exchangeData = sorted(exchangeData, key=lambda exchange: exchange.price)
+    exchangeData = sorted(exchangeData, key=lambda exchange: exchange.bid)
     lowest = exchangeData[0]
     global USD
     global BTC
 
-    BTCPreFee = USD/float(lowest.price)
+    BTCPreFee = USD/float(lowest.bid)
     BTC = BTCPreFee - (BTCPreFee * lowest.fee)
     USD = 0
-    print 'BUY: '+str(BTC)+' @ $'+str(lowest.price)+' on '+lowest.name+'\n'
+    print 'BUY: '+str(BTC)+' @ $'+str(lowest.bid)+' on '+lowest.name+'\n'
 
 def calculateSellDifference(exchangeData):
-    exchangeData = sorted(exchangeData, key=lambda exchange: exchange.price)
+    exchangeData = sorted(exchangeData, key=lambda exchange: exchange.ask)
     highest = exchangeData[len(exchangeData)-1]
     global USD
     global BTC
-    USDPreFee = float(highest.price) * BTC
+    USDPreFee = float(highest.ask) * BTC
     USD = USDPreFee - (USDPreFee * highest.fee)
-    print 'SELL: '+str(BTC)+' @ $'+str(highest.price)+' on '+highest.name+'\n'
+    print 'SELL: '+str(BTC)+' @ $'+str(highest.ask)+' on '+highest.name+'\n'
     BTC = 0
+
+def getTotalPortfolio(exchangeData):
+    #TODO: aggregate across all of the trade accounts
+    return 'butts'
 
 s.enter(60, 1, tick, (s,))
 s.run()
